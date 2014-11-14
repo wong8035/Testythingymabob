d890c0c1ffb9215f23a26dd9a10184b519b9939d
drewwild <Drew.G.Wild-1@ou.edu>
2014-11-13 18:13:11 -0600

    Update KilltheDucks.js

--- KilltheDucks.js
+++ KilltheDucks.js
@@ -1,18 +1,34 @@
-var direction;
 
 
-direction = 0;
-while (true) {
+var direction_scan;
+var direction_swim;
+
+direction_swim = 0
+direction_scan = 0;
+var i = 0;
+while (i < 500) {
   if (loc_x() < 25) {
-    swim(0);
+    swim(0,100);
   } else {
     if (loc_x() > 75) {
-      swim(180);
+      swim(180,100);
     }
   }
   
-  while (scan(direction) > 280) {
-    direction = direction + 5;
+  while (scan(direction_scan) > 70) {
+    direction_scan = direction_scan + 5;
   }
-  cannon(direction, scan(direction));
+  cannon(direction_scan, scan(direction_scan));
+  i++
+}
+swim(direction_scan, 100);
+while(true)
+{
+
+  while (scan(direction_scan) > 280) {
+    direction_scan = direction_scan + 5;
+  }
+  cannon(direction_scan, scan(direction_scan));
+  swim(direction_scan, 100)
+  
 }
